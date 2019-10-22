d600316e1d5530c6682f78886dbf1c01e7b75e2c
Eeo Jun <141bytes@gmail.com>
2019-10-22 21:54:51 +0100

    update

--- test.py
+++ test.py
@@ -1,35 +1,37 @@
 from graph import complete_graph, erdos_renyi_graph
 from misra_gries import misra_gries
-from bipartite import random_bipartite_graph, edge_colour_bipartite, \
-        validate_colouring
+from heuristic import vizing_heuristic
+from bipartite import random_bipartite_graph, validate_colouring
 
 
-def test_bipartite():
-    print("Testing for bipartite graphs")
+def test_bipartite(f):
+    print("  Testing for bipartite graphs")
     for _ in range(1000):
         g = random_bipartite_graph()
-        edge_colour_bipartite(g)
+        f(g)
         assert validate_colouring(g)
 
 
-def test_general():
-    print("Testing for K_n")
+def test_general(f):
+    print("  Testing for K_n")
     for n in range(10, 60):
         g = complete_graph(n)
-        misra_gries(g)
+        f(g)
         assert validate_colouring(g)
 
 
-def test_random():
-    print("Testing for G(n,p)")
+def test_random(f):
+    print("  Testing for G(n,p)")
     for n in range(10, 60):
         for p in [0.1, 0.2, 0.4, 0.8, 0.9]:
             g = erdos_renyi_graph(n, p)
-            misra_gries(g)
+            f(g)
             assert validate_colouring(g)
 
 
 if __name__ == '__main__':
-    test_bipartite()
-    test_general()
-    test_random()
+    for f in [misra_gries, vizing_heuristic]:
+        print(f"{f.__name__}:")
+        test_bipartite(f)
+        test_general(f)
+        test_random(f)

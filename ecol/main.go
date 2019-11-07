b23b10b45fe3d9dcf0ddbd73f43e645a0ce38723
Eeo Jun <141bytes@gmail.com>
2019-11-07 20:23:37 +0000

    testing

--- ecol/main.go
+++ ecol/main.go
@@ -60,8 +60,13 @@ func main() {
 	for n := 0; n <= 200; n++ {
 		G := complete_graph(n)
 		vizing_heuristic(G)
-		//counting_heuristic_colour(G)
 		if !validate_colouring(G) {
+			fmt.Println("FAIL", n)
+			break
+		}
+		H := complete_graph(n)
+		counting_heuristic_colour(H)
+		if !validate_colouring(H) {
 			fmt.Println("FAIL", n)
 			break
 		}

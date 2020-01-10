1a2aa4f7d01aa10683b61fe032c2b9c7d99903e1
Eeo Jun <141bytes@gmail.com>
2020-01-10 19:14:08 +0000

    clean up graph6 code

--- ecol/graph6.go
+++ ecol/graph6.go
@@ -24,7 +24,7 @@ func get_size(data []byte) (cursor int, size int) {
 		cursor = 8
 	}
 	for i := m; i < cursor; i++ {
-		size += int(data[i]) - 63
+		size += int(data[i] - 63)
 	}
 	return
 }
@@ -33,13 +33,17 @@ func get_graph(data []byte, size int) *Graph {
 	graph := NewGraph(size)
 	k := 0
 	// Generate ordering (0,1),(0,2),(1,2),(0,3),(1,3),(2,3),...
-	// Might look weird but consider: (1,0),(2,0),(2,1),(3,0),(3,1),...
-	for j := 0; j < size; j++ {
-		for i := 0; i < j; i++ {
+	// Might look weird but consider:
+	//   (1,0),
+	//   (2,0),(2,1),
+	//   (3,0),(3,1),(3,2),
+	//   ...
+	for v := 0; v < size; v++ {
+		for u := 0; u < v; u++ {
 			b := (data[k/6] - 63) << 2  // relevant byte
 			m := byte(1 << uint(7-k%6)) // mask
 			if (b & m) != 0 {
-				graph.Set(i, j, 0)
+				graph.Set(u, v, 0)
 			}
 			k++
 		}

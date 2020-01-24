639cd0630e12c3307f4292a16ecb753cc7a111c6
Eeo Jun <141bytes@gmail.com>
2020-01-24 22:22:17 +0000

    remove script.sh

--- script.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-cat ./class2_12.jsonl | xc/resmod 0 4 | xc/gc -u | xc/xc 1500 > class2_12.1.jsonl &
-cat ./class2_12.jsonl | xc/resmod 1 4 | xc/gc -u | xc/xc 1500 > class2_12.2.jsonl &
-cat ./class2_12.jsonl | xc/resmod 2 4 | xc/gc -u | xc/xc 1500 > class2_12.3.jsonl &
-cat ./class2_12.jsonl | xc/resmod 3 4 | xc/gc -u | xc/xc 1500 > class2_12.4.jsonl &
-wait

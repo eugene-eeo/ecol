d214acf82095776aeb0b6f208ecddd8c804cfc7c
Eeo Jun <141bytes@gmail.com>
2020-02-25 21:20:31 +0000

    benchmark

--- lxc/benchmark.sh
+++ lxc/benchmark.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for file in pts/*.pt; do
-    if [ "$file" != "pts/qg.order100.pt" ]; then
+    # if [ "$file" != "pts/qg.order100.pt" ]; then
         output=$(./lxc -p -a1 < "$file")
         echo "$file,$output"
-    fi
+    # fi
 done

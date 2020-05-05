0611e1ddd150ca1a8fa88995eaf180575e16b553
Eeo Jun <141bytes@gmail.com>
2020-05-05 11:38:31 +0100

    update

--- scripts/relabel.py
+++
@@ -1,9 +0,0 @@
-import sys
-import json
-
-
-if __name__ == '__main__':
-    for line in sys.stdin:
-        u = json.loads(line)
-        edge_data = u["edge_data"]
-        print(f"{len(edge_data)}|{line.strip()}")
